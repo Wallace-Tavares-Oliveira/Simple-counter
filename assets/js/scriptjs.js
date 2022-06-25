a56312459f51cf0f6d94bc43ce4608358072dd55
Wallace <wallacetavaresoliveira84@gmail.com>
2022-06-24 21:34:22 -0300

    refactoring code

--- assets/js/scriptjs.js
+++ assets/js/scriptjs.js
@@ -1,17 +1,23 @@
 // get field content with (id)
 let currentNumnerWrapper = document.querySelector('#curentNumber');
+let out = document.getElementById('out');
 
 currentNumner = 0;
 // declare function
 function increment() {
-    
     currentNumner = currentNumner + 1;
     document.getElementById('curentNumber').innerHTML = currentNumner;
+    if(currentNumner > 10) {
+        document.getElementById('out').innerText = "The number is greater than 10";
+    }
 }
 
 function decrement() {
     currentNumner = currentNumner - 1;
     document.getElementById('curentNumber').innerHTML = currentNumner;
+    if(currentNumner < 0) {
+        document.getElementById('out').innerText = "The count is decreasing...";
+    }
 }
 
 currentNumner.addEventListener('click', increment);
